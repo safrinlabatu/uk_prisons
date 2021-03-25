f7a4ee14e6d23eebf41797ae31eea5ce33f7e93b
Safrin La Batu <batus@cardiff.ac.uk>
2021-03-25 01:30:30 +0000

    Adding image on-scroll functionality

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 var onScrollHandler = function() {
-	var newImageUrl = yourImageElement.src;
+	var newImageUrl = 'img src="img/viz_1.jpg" alt=""';
 	var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 	if (scrollTop > 100) {
-     newImageUrl = "img1.jpg"
+     newImageUrl = 'img src="img/viz_2.jpg" alt=""'
 	}
   	if (scrollTop > 200) {
-     newImageUrl = "img2.jpg"
+     newImageUrl = 'img src="img/viz_3.jpg" alt=""'
   	}
   	if (scrollTop > 300) {
-     newImageUrl = "img3.jpg"
+     newImageUrl = 'img src="img/viz_3.jpg" alt=""'
 	}
- 	yourImageElement.src = newImageUrl;
+ 	'img src="img/viz_1.jpg" alt=""' = newImageUrl;
 };
 object.addEventListener ("scroll", onScrollHandler);
