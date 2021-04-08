ff0024daff606a98a52cca9440d1ebbafc7703ba
Safrin La Batu <batus@cardiff.ac.uk>
2021-04-08 12:57:30 +0100

    adding buttons

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 var onScrollHandler = function() {
-	var newImageUrl = 'img src="img/viz_1.jpg" alt=""';
+	var newImageUrl = img.src;
 	var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 	if (scrollTop > 100) {
-     newImageUrl = 'img src="img/viz_2.jpg" alt=""'
+     newImageUrl = "img/viz_2.jpg" 
 	}
   	if (scrollTop > 200) {
-     newImageUrl = 'img src="img/viz_3.jpg" alt=""'
+     newImageUrl = "img/viz_3.jpg" 
   	}
   	if (scrollTop > 300) {
-     newImageUrl = 'img src="img/viz_3.jpg" alt=""'
+     newImageUrl = "img/viz_3.jpg" 
 	}
- 	'img src="img/viz_1.jpg" alt=""' = newImageUrl;
+ 	"img/viz_1.jpg" = newImageUrl;
 };
 object.addEventListener ("scroll", onScrollHandler);
+
+/* alternative */ 
+
+
