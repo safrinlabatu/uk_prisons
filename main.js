c037b3b8acc1f3d58942aa49ac0dde592335ce2c
Safrin La Batu <batus@cardiff.ac.uk>
2021-05-09 19:54:12 +0100

    Adding a new look

--- main.js
+++ main.js
@@ -1,19 +1,1 @@
-var onScrollHandler = function() {
-	var newImageUrl = img.src;
-	var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-	if (scrollTop > 100) {
-     newImageUrl = "img/viz_2.jpg" 
-	}
-  	if (scrollTop > 200) {
-     newImageUrl = "img/viz_3.jpg" 
-  	}
-  	if (scrollTop > 300) {
-     newImageUrl = "img/viz_3.jpg" 
-	}
- 	"img/viz_1.jpg" = newImageUrl;
-};
-object.addEventListener ("scroll", onScrollHandler);
-
-/* alternative */ 
-
 
